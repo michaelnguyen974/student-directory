9ee6a790aa0b9e662a58883d88affe75f7bb7a52
Michael Nguyen <michaelnguyen974@gmail.com>
2018-11-20 13:04:09 +0000

    Print the list of students

--- directory.rb
+++ directory.rb
@@ -1,13 +1,16 @@
-puts "The students of Villains Academy"
-puts "_____________"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+puts ("The students of Villains Academy")
+puts ("_____________")
+puts ("Dr. Hannibal Lecter")
+puts ("Darth Vader")
+puts ("Nurse Ratched")
+puts ("Michael Corleone")
+puts ("Alex DeLarge")
+puts ("The Wicked Witch of the West")
+puts ("Terminator")
+puts ("Freddy Krueger")
+puts ("The Joker")
+puts ("Joffrey Baratheon")
+puts ("Norman Bates")
+print "Overall, we have "
+print 11 
+puts " great students"
