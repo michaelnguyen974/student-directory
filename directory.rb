73ad794f1c9874b97a501c6a89c234e79df5458b
Michael Nguyen <michaelnguyen974@gmail.com>
2018-11-20 17:38:09 +0000

    added asking user for input to create a new array of students that will have a name and a cohort

--- directory.rb
+++ directory.rb
@@ -27,6 +27,21 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end 
 
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit enter twice"
+	students = []
+	name = gets.chomp 
+
+	while !name.empty? do 
+		students << {name: name, cohort: :november}
+		puts "Now we have #{students.count} students"
+		name = gets.chomp
+	end
+	students
+end 
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
